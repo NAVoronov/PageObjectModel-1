49e06cdb83cfa71b9ab1f7e8896f0b954dec5052
NAVoronov <nixvoronov@gmail.com>
2020-11-23 22:40:09 +0300

    добавил проверку странички логина на наличие форм и в названии её login должен присутствовать также

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,25 @@
 # сам тест
 
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+	link = "http://selenium1py.pythonanywhere.com/"
+	page = MainPage(browser, link)
+	page.open()
+	page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_and_login_test(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+    log_page = LoginPage(browser, browser.current_url)
+    log_page.should_be_login_page()
